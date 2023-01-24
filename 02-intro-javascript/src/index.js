338ae77447c31d2e7497f58215e6d7eb94a4c5fd
israel <israel.zacarias@plasticosuni.com>
2023-01-24 16:29:29 -0600

    se estudian los objetos literales

--- 02-intro-javascript/src/index.js
+++ 02-intro-javascript/src/index.js
@@ -3,17 +3,32 @@
 // objetos literales
 
 const persona = {
-// llave    //valor
+// llave      //valor
     nombre: 'Israel',
     apellido: 'Aveiro',
     edad: 38,
+    direccion: {
+        ciudad: 'New York',
+        zip: 5533456,
+        lat: 14.334,
+        lng: 35.67895
+    }
 };
 
 
 // recuperar el nombre de Israel 
 console.log( persona.nombre );
 
-// creamos un nuevo objeto
+// creamos un nuevo objeto que tiene una propiedad llamada persona y el valor al que apunta es al objeto
 console.log( {
+// propiedad  //valor
     persona:persona
 } );
+
+// lo podemos visualizar en una tabla
+console.table( {
+    // propiedad  //valor
+        persona:persona
+    } );
+    
+    
