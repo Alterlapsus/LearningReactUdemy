e6bc50630879fd24f7cee94bf37b46a1e8f79546
israel <israel.zacarias@plasticosuni.com>
2023-01-26 10:13:05 -0600

    Se agrega contenido de objetos literales

---
+++ 02-intro-javascript/src/bases/03-objeto-literal.js
@@ -0,0 +1,45 @@
+
+
+// objetos literales
+
+const persona = {
+// llave      //valor
+    nombre: 'Israel',
+    apellido: 'Aveiro',
+    edad: 38,
+    direccion: {
+        ciudad: 'New York',
+        zip: 5533456,
+        lat: 14.334,
+        lng: 35.67895
+    }
+};
+
+// recuperar el nombre de Israel 
+console.log( persona.nombre );
+
+// creamos un nuevo objeto que tiene una propiedad llamada persona y el valor al que apunta es al objeto
+console.log( {
+    // propiedad  //valor
+    // persona:persona
+} );
+
+// lo podemos visualizar en una tabla
+console.table( {
+    // propiedad  //valor
+    // persona:persona
+    } );
+    
+
+// si queremos crear otro objeto clonado realizamos una asignación de referencia es decir asignamos los parametros y valores del objeto literal persona a persona2
+
+// No debemos de realizar este tipo de mutaciones
+// const persona2 = persona;
+
+// Forma correcta de crear un clon utilizando spread que son ... y el nombre del objeto que se quiere clonar
+const persona2 = { ...persona }
+// cambiar valores de los parametros del objeto
+persona2.nombre = 'Luis'; 
+
+console.log( persona );
+console.log( persona2 );
